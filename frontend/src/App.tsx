e9f65210c47d3fe63fe5d80ec4972bdecd8e44e8
Carlos lorenzo <clorenzozuniga@gmail.com>
2025-03-31 20:04:12 +0200

    - Basic project setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import {
+    Routes,
+    Route,
+} from "react-router-dom";
+
+import Home from './pages/Home.tsx';
+
+function App() {
+  return (
+	<>
+		<Routes>
+			<Route path="/" element={<Home/>} />
+		</Routes>
+	</>
+
+  )
+}
+
+export default App
